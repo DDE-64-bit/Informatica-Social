fa59f937d0df823f2e1bfac873d5bcdeec50cfdf
DDE <119262202+DDE-64-bit@users.noreply.github.com>
2024-01-22 21:01:33 +0000

    changes (stable)

--- ReadMe.md
+++ ReadMe.md
@@ -182,10 +182,55 @@ path('signup', views.signup, name='signup'),
 open /core/views.py
 
 voeg deze functie toe: 
+
 def signup(request):
-    return render(request, 'signup.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+        
+    else:
+        return render(request, 'signup.html')
+
+
+
+en importeer deze libery's
+
+from django.shortcuts import redirect
+from django.contrib .auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
+
 
 start nu je server en ga naar 127.0.0.1:4000/signup zie je iets?
+probeer eens een account aan te maken. ga dan naar 127.0.0.1:4000/admin en log in met je superuser
+druk dan op profiles. zie jij jouw account staan?
 
 ga naar core/views.py
 
--- project/social1/core/views.py
+++ project/social1/core/views.py
@@ -4,13 +4,15 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib .auth.models import User, auth
 from django.contrib import messages
+from .models import Profile
 
 
 def index(request):
     return render(request, "index.html")
 
 def signup(request):
-    if request.method ==  'POST':
+
+    if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
@@ -18,16 +20,27 @@ def signup(request):
 
         if password == password2:
             if User.objects.filter(email=email).exists():
-                messages.info(request, "There is already an user with this email")
-                return redirect("signup")
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
             elif User.objects.filter(username=username).exists():
-                messages.info(request, "There is someone with this username")
-                return redirect("signup")
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
-            messages.info(request, 'Passwords Not Matching')
+            messages.info(request, 'Password Not Matching')
             return redirect('signup')
+        
     else:
         return render(request, 'signup.html')
