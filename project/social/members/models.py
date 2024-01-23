d20f83ab67d76852d5f5fbb33cbd2ee319344b80
DDE <119262202+DDE-64-bit@users.noreply.github.com>
2024-01-23 10:52:39 +0000

    deleted social

--- project/social/members/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Member(models.Model):
-  voorNaam = models.CharField(max_length=255)
-  achterNaam = models.CharField(max_length=255)
--- project/social/members/urls.py
+++
@@ -1,8 +0,0 @@
-# members
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('members/', views.members, name='members'),
-]
